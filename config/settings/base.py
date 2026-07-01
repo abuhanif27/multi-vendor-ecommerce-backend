36ab90b6cb374f9e0e765b0063871b286b93effc
Abu Hanif <ahanif2500@gmail.com>
2026-07-02 04:01:51 +0600

    Add initial structure for accounts, catalog, common, notifications, orders, payments, and shops apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -30,15 +30,32 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
+THIRD_PARTY_APPS = []
+
+LOCAL_APPS = [
+    "common",
+    "accounts",
+    "shops",
+    "catalog",
+    "orders",
+    "payments",
+    "notifications",
+]
+
+INSTALLED_APPS = (
+    DJANGO_APPS
+    + THIRD_PARTY_APPS
+    + LOCAL_APPS
+)
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
