16d05c683a7d1da94b1fc4ecd25ad19b71350a5b
Abu Hanif <ahanif2500@gmail.com>
2026-07-03 05:54:07 +0600

    Implement user registration response message in RegisterAPIView

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -5,3 +5,14 @@ from apps.accounts.serializers import RegisterSerializer
 
 class RegisterAPIView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response(
+            {
+
+                "message": "User Registration Successful. Please verify your email.",
+            }
+        )
