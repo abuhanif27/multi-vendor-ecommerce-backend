90c10e1acab2df2bf940acf11f5b66be34f991e0
Abu Hanif <ahanif2500@gmail.com>
2026-07-03 04:16:42 +0600

    Add UserAdmin class to manage User model in Django admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from apps.accounts.models import User
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "role", "is_staff", "is_active", "date_joined")
+    list_filter = ("role", "is_staff", "is_active")
+    search_fields = ("email",)
+    ordering = ("email",)
