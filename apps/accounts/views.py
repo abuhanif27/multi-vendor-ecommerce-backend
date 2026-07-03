60ecd1ec74fc84a9687b75039b7af688daa17ac0
Abu Hanif <ahanif2500@gmail.com>
2026-07-04 03:21:57 +0600

    Add email verification token generation in RegisterAPIView

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
-
+from rest_framework.response import Response
 from apps.accounts.serializers import RegisterSerializer
+from apps.accounts.models import EmailVerificationToken
 
 
 class RegisterAPIView(generics.CreateAPIView):
@@ -9,7 +10,16 @@ class RegisterAPIView(generics.CreateAPIView):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
+        user = serializer.save()
+
+        token = EmailVerificationToken.objects.create(user=user)
+
+        print("=" * 80)
+        print("EMAIL VERIFICATION LINK")
+        print(
+            f"http://127.0.0.1:8000/api/v1/auth/verify-email/?token={token.token}")
+        print("=" * 80)
+
         return Response(
             {
 
