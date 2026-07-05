8692733d02623c4f80d623dc8e537a9138639d23
Abu Hanif <ahanif2500@gmail.com>
2026-07-06 02:37:51 +0600

    Refactor create_verification_token function to return the created token directly

--- apps/accounts/services.py
+++ apps/accounts/services.py
@@ -2,5 +2,4 @@ from apps.accounts.models import EmailVerificationToken
 
 
 def create_verification_token(user):
-    token = EmailVerificationToken.objects.create(user=user)
-    return token
+    return EmailVerificationToken.objects.create(user=user)
