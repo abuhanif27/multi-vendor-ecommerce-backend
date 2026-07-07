b9c5a393af93d16eba217dbfc7536c500972ab84
Abu Hanif <ahanif2500@gmail.com>
2026-07-07 16:05:12 +0600

    Add LoginSerializer for user authentication with email and password validation

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
@@ -36,3 +37,26 @@ class RegisterSerializer(serializers.Serializer):
         validated_data.pop("confirm_password", None)
 
         return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        user = authenticate(
+            email=email,
+            password=password,
+        )
+
+        if user is None:
+            raise serializers.ValidationError(
+                "Invalid email or password."
+            )
+
+        attrs["user"] = user
+
+        return attrs
