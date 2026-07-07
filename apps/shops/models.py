ec5634eeb1cdc0abc78ff03fc2ca471ed6116c6f
Abu Hanif <ahanif2500@gmail.com>
2026-07-07 17:17:26 +0600

    Refactor slug generation logic in Shop model's save method for clarity and correctness

--- apps/shops/models.py
+++ apps/shops/models.py
@@ -34,5 +34,14 @@ class Shop(UUIDModel, TimeStampedModel):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.name)
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 2
+
+        while Shop.objects.filter(slug=slug).exists():
+            slug = f"{base_slug}-{counter}"
+            counter += 1
+
+        self.slug = slug
+
         super().save(*args, **kwargs)
