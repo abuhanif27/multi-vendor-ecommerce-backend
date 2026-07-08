0ed03d49c21dee8be9066204754adbd1404fa4f4
Abu Hanif <ahanif2500@gmail.com>
2026-07-09 02:57:01 +0600

    Fix Product model save method to handle slug generation correctly with blank description

--- apps/shops/models.py
+++ apps/shops/models.py
@@ -74,7 +74,7 @@ class Product(UUIDModel, TimeStampedModel):
         unique=True,
     )
 
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
     price = models.DecimalField(
         max_digits=10,
@@ -94,17 +94,16 @@ class Product(UUIDModel, TimeStampedModel):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 2
 
-def save(self, *args, **kwargs):
-    if not self.slug:
-        base_slug = slugify(self.name)
-        slug = base_slug
-        counter = 2
+            while Product.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
 
-        while Product.objects.filter(slug=slug).exists():
-            slug = f"{base_slug}-{counter}"
-            counter += 1
+            self.slug = slug
 
-        self.slug = slug
-
-    super().save(*args, **kwargs)
+        super().save(*args, **kwargs)
