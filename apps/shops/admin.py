7dfecaf2259bb4f32ace07d42045981912908dcb
Abu Hanif <ahanif2500@gmail.com>
2026-07-13 03:17:59 +0600

    Register Product and ProductImage models in the admin interface

--- apps/shops/admin.py
+++ apps/shops/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from apps.shops.models import Shop
+from apps.shops.models import Shop, Product, ProductImage
 # Register your models here.
 
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
     readonly_fields = ("slug", )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ("slug", )
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    pass
