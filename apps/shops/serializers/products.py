6415dcd4d78a16563c93825333f0014ae46e3dd4
Abu Hanif <ahanif2500@gmail.com>
2026-07-14 00:24:38 +0600

    Refactor serializers: remove ProductImageSerializer and reorganize Shop and Product serializers

---
+++ apps/shops/serializers/products.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+
+from apps.shops.models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            "id",
+            "shop",
+            "category",
+            "name",
+            "slug",
+            "description",
+            "price",
+            "stock",
+            "status",
+        )
+
+        read_only_fields = (
+            "id",
+            "slug",
+            "status",
+        )
+
+    def validate_shop(self, shop):
+        request = self.context.get("request")
+        if request.user != shop.owner:
+            raise serializers.ValidationError(
+                "You do not have permission to add a product to this shop."
+            )
+        return shop
+
+    def validate_category(self, category):
+        if not category.is_active:
+            raise serializers.ValidationError(
+                "You cannot add a product to an inactive category."
+            )
+        return category
